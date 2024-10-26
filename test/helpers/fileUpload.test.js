60f691e345b08a85a670927cedc9f8b407a19fd7
Jair Quispe <quispejair492@gmail.com>
2024-10-25 23:04:13 -0500

    fix firebase lite

--- test/helpers/fileUpload.test.js
+++ test/helpers/fileUpload.test.js
@@ -30,7 +30,6 @@ describe('Pruebas en fileUpload', () => {
         const cloudResp = await cloudinary.api.delete_resources([imageId], {
             resource_type: 'image'
         })
-        console.log({cloudResp})
 
 
 
---
+++ test/store/auth/thunks.test.js
@@ -0,0 +1,23 @@
+import { checkingAuthentication } from "../../../src/store/auth/thunks"
+
+jest.mock('../../../src/firebase/providers')
+
+
+describe('Pruebas en AuthThu ', () => {
+
+    const dispatch = jest.fn()
+    beforeEach(()=> jest.clearAllMocks())
+
+
+    test('debe invocar el checkingCredentials', async() => {
+
+
+        await checkingAuthentication()(dispatch)
+        expect(dispatch).toHaveBeenCalledWith(checkingAuthentication())
+
+
+
+    })
+
+
+})
